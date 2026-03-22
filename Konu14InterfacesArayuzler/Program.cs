67429366bd5cf09093bdaada1c4d0dad54a806fa
arcadev-ops <arca.dev@icloud.com>
2026-03-22 16:06:35 +0300

    ilk yukleme konu 01 ve 02

---
+++ Konu14InterfacesArayuzler/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Konu14InterfacesArayuzler
+{
+    public interface OrnekArayuz // class yerine interface kelimesini kullanarak tanımlıyoruz
+    {
+        public int Id { get; set; }
+    }
+    interface IDemo
+    {
+        void Goster(); // metot imzası
+    }
+    interface icerebilecekleri : IDemo // interface ler başka interfacelerden miras alabilir.
+    {
+        // bu interface i kullanacak olan classlar aşağıdaki varlıkları kullanmak zorundadır.
+        public int sayi { get; set; }
+        public static int sayi2 { get; set; }
+        // interface lerde metotlar değil sadece imzaları yer alır.
+        void Topla(); // geri değer döndürmeyen metot imzası
+        int ToplamaYap(); // geri int değer döndürmesi gereken metot imzası
+    }
+    class ArayuzKullanimi : icerebilecekleri // bu şekilde miras aldığımızda interface ler içerisindeki varlıklar class ta kullanılmak zorundadır.
+    {
+        public int sayi { get; set; }
+        public int Id { get; set; } // class içerisinde interface de olmayan ama class ta olması gereken property veya metotlar bulunabilir.
+        public void Goster() // interface de imzası bulunan metot class içinde kullanılmak zorundadır!
+        {
+            Console.WriteLine("void Goster metodu");
+        }
+
+        public void Topla()
+        {
+            Console.WriteLine("void Topla metodu");
+        }
+
+        public int ToplamaYap()
+        {
+            return sayi + 1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konu14InterfacesArayuzler");
+            ArayuzKullanimi arayuz = new ArayuzKullanimi();
+            arayuz.sayi = 1;
+            arayuz.Topla();
+            arayuz.Goster();
+            Console.WriteLine("Toplama sonucu: " + arayuz.ToplamaYap());
+
+            Console.WriteLine();
+
+            Kategori kategori = new Kategori()
+            {
+                Id = 1,
+                Name = "Elektronik",
+                IsActive = true
+            };
+
+            Console.WriteLine("Kategori");
+            Console.WriteLine(kategori.Name);
+
+            Console.WriteLine();
+
+            Urun urun = new Urun()
+            {
+                Id = 1,
+                Name = "Ayfon Çift Sim Cep Telefonu",
+                IsActive = true,
+                Price = 18000
+            };
+            Console.WriteLine("Ürün Bilgileri:");
+            Console.WriteLine(urun.Name);
+            Console.WriteLine(urun.Price + " TL");
+        }
+    }
+}
---
+++ Konu16CollectionsKoleksiyonlar/Program.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections; // Koleksiyonları kullanabilmek için gerekli kütüphane
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Konu16CollectionsKoleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Konu16CollectionsKoleksiyonlar");
+            // Ornek1();
+            // Ornek2();
+            // Ornek3();
+            // Ornek4();
+            // Ornek5();
+            ListKullanimi();
+        }
+        static void Ornek1()
+        {
+            ArrayList arrayList = new();
+            arrayList.Add(1);
+            arrayList.Add("iki");
+            arrayList.Add(3.0);
+            arrayList.Add(true);
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ArrayList ilk eleman : " + arrayList[0]);// listede index ini verdiğimiz elemana ulaşma
+        }
+        static void Ornek2()
+        {
+            ArrayList arrayList = new();
+            arrayList.Add("İstanbul");
+            arrayList.Add("Ankara");
+            arrayList.Add("İzmir");
+            arrayList.Add("Sivas");
+            arrayList.Add("Çankırı");
+            arrayList.Add("Zonguldak");
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("ArrayList de sıralama yapabiliriz");
+
+            arrayList.Sort(); // a-z sıralama
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            arrayList.Reverse(); // tersten sırala
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+        }
+        static void Ornek3()
+        {
+            var strings = new StringCollection();
+            strings.Add("İstanbul");
+            strings.Add("Ankara");
+            strings.Add("Bursa");
+            // strings.Add(34); // StringCollection a sadece string veriler eklenebilir
+            Console.WriteLine("StringCollection");
+            foreach (var item in strings)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Ornek4()
+        {
+            var dictionary = new StringDictionary(); // key value şeklinde veri saklayabilir
+            dictionary.Add("18", "Çankırı");
+            dictionary.Add("06", "Angara");
+            dictionary.Add("34", "İstanbul");
+            dictionary.Add("01", "Adana");
+            dictionary.Add("58", "Sivas");
+            Console.WriteLine();
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("dictionary.Keys:");
+            foreach (var item in dictionary.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("dictionary.Values:");
+            foreach (var item in dictionary.Values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Ornek5()
+        {
+            Dictionary<string, string> dictionary = new(); // hangi veri tipinden oluşacağını bizim belirleyebildiğimiz liste sistemi
+            dictionary.Add("book", "kitap");
+            dictionary.Add("18", "Çankırı");
+            dictionary.Add("34", "İstanbul");
+            Console.WriteLine(dictionary["book"]);
+
+            Console.WriteLine();
+
+            Dictionary<int, string> dictionary2 = new();
+            dictionary2.Add(1, "Adana");
+            dictionary2.Add(18, "Çankırı");
+            dictionary2.Add(34, "İstanbul");
+
+            Console.WriteLine("dictionary2 Values: ");
+
+            foreach (var item in dictionary2)
+            {
+                Console.WriteLine(item.Value);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("dictionary2 Keys: ");
+
+            foreach (var item in dictionary2)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+        static void ListKullanimi()
+        {
+            Console.WriteLine("List Kullanimi");
+            List<string> sehirler = new(); // string veri tipi alabilen bir liste
+            sehirler.Add("Ankara");
+            sehirler.Add("İstanbul");
+            sehirler.Add("Kocaeli");
+            sehirler.Add("Sivas 58");
+            sehirler.Add("Çankırı");
+            // sehirler.Add(18); // !! List<string> dediğimiz için string dışında listeye ekleme yapamayız!
+            Console.WriteLine("Şehirler:");
+            foreach (var item in sehirler) // sehirler isimli listede dön
+            {
+                Console.WriteLine(item); // listedeki her nesneyi ekrana yaz
+            }
+            Console.WriteLine();
+
+            List<User> users = new();
+            users.Add(new User
+            {
+                Id = 1,
+                Name = "Mesut",
+                Email = "mesut",
+                Password = "123"
+            });
+            users.Add(new User
+            {
+                Id = 2,
+                Name = "Alp",
+                Email = "alp",
+                Password = "321"
+            });
+            Console.WriteLine("Kullanıcılar:");
+            foreach (var item in users)
+            {
+                Console.WriteLine(item.Name + " " + item.Password);
+            }
+            Console.WriteLine();
+            List<User> kullanicilar = new()
+            {
+                new User
+                {
+                    Id = 3,
+                    Name = "Pusat",
+                    Email = "pusat",
+                    Password = "gmr12"
+                },
+                new User
+                {
+                    Id = 4,
+                    Name = "Murat",
+                    Email = "yilmaz",
+                    Password = "mry25"
+                }
+            };
+            Console.WriteLine("Kullanıcılar 2:");
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item.Name + " " + item.Password);
+            }
+            Console.WriteLine();
+            var yeniKullanici = new User
+            {
+                Id = 5,
+                Name = "Aslan",
+                Password = "789"
+            };
+            Console.WriteLine("Kullanıcılar listesinde yeniKullanici var mı? :");
+            var varmi = kullanicilar.Contains(yeniKullanici); // Contains metoduyla bir listede arama yapabiliriz.
+            Console.WriteLine("varmi ? " + varmi);
+            kullanicilar.Add(yeniKullanici);
+            Console.WriteLine("şimdi varmı? " + kullanicilar.Contains(yeniKullanici));
+            Console.WriteLine();
+            Console.WriteLine("Kullanıcılar 3:");
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item.Name + " " + item.Password);
+            }
+            Console.WriteLine();
+            kullanicilar.AddRange(users); // AddRange metodu listeye çoklu kayıt eklememizi sağlar.
+            kullanicilar.Insert(0, yeniKullanici); // Insert metodu listeye verdiğimiz indexe ekleme yapmamızı sağlar
+            Console.WriteLine();
+            Console.WriteLine("Kullanıcılar 4:");
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine(item.Name + " " + item.Password);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Listedeki kayıt sayısı: " + kullanicilar.Count); // Count listedeki eleman sayısını getirir.
+        }
+    }
+}
